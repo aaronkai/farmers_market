d8e01027ff753341b2533263f874a3cdf531294e
Aaron Hubbard <aaron@aaron-laptop.(none)>
2013-06-01 15:01:19 -0400

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ FarmersMarket::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
-  root :to => 'application#hello'
+  root :to => 'map#index'
   
   resources :markets, :vendors
 end
