aaab0bd98f43f16594c813c1b322f485a6e594b7
Aaron Hubbard <aaron@aaron-laptop.(none)>
2013-06-01 15:23:19 -0400

    added map to market view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ FarmersMarket::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
-  root :to => 'map#index'
+  root :to => 'markets#index'
   
   resources :markets, :vendors
 end
